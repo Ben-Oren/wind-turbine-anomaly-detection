1dc0dac4dbd30fc3b875debbe5807795e42c7286
Ben-Oren <oren.ben.k@gmail.com>
2018-12-20 14:18:03 -0600

    setting up cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='detecting anomalies in wind turbine SCADA operational data',
+    author='Ben Oren',
+    license='BSD-3',
+)
